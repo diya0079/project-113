cfec00cf12d3bf269d89c12857492df5ad23f651
diya0079 <86662268+diya0079@users.noreply.github.com>
2021-10-12 17:11:42 +0530

    Add files via upload

---
+++ project-113/main.js
@@ -0,0 +1,22 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas= createcanvas(640,480);
+    canvas.position(110,250);
+    video= createcapture(VIDEO);
+    video.hide();
+
+    tint_color= "";
+}
+ funtion draw() {
+     image(video, 0, 0, 640, 480);
+     tint(tint_color);
+ }
+
+ funtion take_snapshot() {
+save('student_name.png');
+
+ 
+ 
